4928942e1d40229ea47fbeb8133e088d145d5170
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-09 17:56:45 -0600

    created two header divs for the header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,13 @@ function Header() {
     return (
         <div className="header"> 
             <h2>This is the header</h2>
-            
+            <div className="header_left">
+
+            </div>
+            <div className="header_right">
+
+            </div>
+             
         </div>
     )
 }
