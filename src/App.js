718fa8edc590005de4d387bef65c54dde2d1da07
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-09 22:33:50 -0600

    added sidebat to the app js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
