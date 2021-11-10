8dea977525d7633240e59296b69a9ddeea7afa92
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-10 13:46:47 -0600

    added a firebase js file

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import firebase from "firebase"
 
-function Firebase() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWDanrPIZz-yfiyTn1fSo-VgmSqM8CyMQ",
+  authDomain: "linkedin-clone-prjc.firebaseapp.com",
+  projectId: "linkedin-clone-prjc",
+  storageBucket: "linkedin-clone-prjc.appspot.com",
+  messagingSenderId: "917998464269",
+  appId: "1:917998464269:web:ac2cc535b1be4fe5b3d444",
+  measurementId: "G-NQ1K4SFHQY"
+};
 
-export default Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig)
