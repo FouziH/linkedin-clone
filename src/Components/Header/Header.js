85b3a33dffc720932e8115095e175129ca045be6
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-09 18:54:12 -0600

    installed material ui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className="header"> 
             <h2>This is the header</h2>
             <div className="header_left">
+                <img src="" alt=""/>
+                <div className="header_search">
+                    {/* SeatchIcon */}
+                    <SearchIcon />
+
+                    <input type="text"/>
+                </div>
 
             </div>
             <div className="header_right">
