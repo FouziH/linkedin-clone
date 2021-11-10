ffe9478eaf6a714436b2bb074d95a255b453718d
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-10 12:53:25 -0600

    importing icons and resuable component into post component

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,6 +1,11 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 import "../Post/Post.css";
+import InputOption from "../InputOption/InputOption";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 
 function Post({ name, description, message, photoUrl }) {
   return (
@@ -8,13 +13,18 @@ function Post({ name, description, message, photoUrl }) {
       <div className="post_header">
         <Avatar />
         <div className="post_info">
-          <h2>Hamza Mohamed</h2>
-          <p>Description</p>
+          <h2>{name}</h2>
+          <p>{description}</p>
         </div>
       </div>
       <div className="post_body">
-         {message? message : <p>Message goes here</p>}
-
+        {message ? message : <p>Message goes here</p>}
+      </div>
+      <div className="post_buttons">
+        <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="grey" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="grey" />
+        <InputOption Icon={ShareOutlinedIcon} title="Share" color="grey" />
+        <InputOption Icon={SendOutlinedIcon} title="Like" color="grey" />
       </div>
     </div>
   );
