3a23a090fda622ad59d7b555b6f28ded85e71ed2
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-13 22:49:28 -0600

    created 4 inputs

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../Login/Login'
+import '../../../public/linkedin.svg'
 
 export default function Login() {
     return (
-        <div>
-            
+        <div className="login">
+            <img src="https://cdn.worldvectorlogo.com/logos/linkedin.svg" alt=""/>
+            <form>
+                <input />
+                <input />
+                <input />
+                
+
+            </form>
         </div>
     )
 }
