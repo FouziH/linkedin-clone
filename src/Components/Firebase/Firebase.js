2a2b4f09a48040a892a3140d6192fc0275d084cb
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-13 19:25:34 -0600

    created db and auth for firebase

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore'
+import {getAuth} from 'firebase/auth'
 const firebaseConfig  =  {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -12,6 +13,12 @@ const firebaseConfig  =  {
 
 const app = initializeApp(firebaseConfig)
 
+const auth = getAuth(app)
 
-export const db = getFirestore(app)
+
+const db = getFirestore(app);
+
+export {db, auth}
+
+
 
