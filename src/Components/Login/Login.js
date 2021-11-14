d2d483ed253f2d7c1cdb4481efd0204c57f292a5
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-13 23:00:01 -0600

    created the form for registering

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import '../Login/Login'
-import '../../../public/linkedin.svg'
+import React from "react";
+import "../Login/Login.css";
 
 export default function Login() {
-    return (
-        <div className="login">
-            <img src="https://cdn.worldvectorlogo.com/logos/linkedin.svg" alt=""/>
-            <form>
-                <input />
-                <input />
-                <input />
-                
+  return (
+    <div className="login">
+      <img src="https://cdn.worldvectorlogo.com/logos/linkedin.svg" alt="" />
+      <form>
+        <input placeholder="Full name (required if registering" type="text" />
+        <input placeholder="Profile pic URL (Optional)" />
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit">Sing in</button>
+      </form>
 
-            </form>
-        </div>
-    )
+      <p>Not a member? 
+
+          <span className="login_register">Register Now</span>
+      </p>
+    </div>
+  );
 }
