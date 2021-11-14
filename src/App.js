58f2cf902fc4516dc19af63e93ef62e5d86f22de
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-13 22:35:02 -0600

    using redux store in app js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import Feed from './Components/Feed/Feed';
 import Header from './Components/Header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
+import { selectUser } from './features/userSlice';
 
 function App() {
 
