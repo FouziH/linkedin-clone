3409efc6e2a12c5c3e4a744407c824772d073130
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-13 23:27:48 -0600

    created local state to compture and hold information for dispatch

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../Firebase/Firebase";
 import "../Login/Login.css";
 
 export default function Login() {
+    const [name, setName] = useState('')
+    const [photoURL, setPhotoURL] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const login = (e) => {
         e.preventDefault()
-        auth
+      
 
     }
     const register = () => {
@@ -15,10 +19,10 @@ export default function Login() {
     <div className="login">
       <img src="https://cdn.worldvectorlogo.com/logos/linkedin.svg" alt="" />
       <form>
-        <input placeholder="Full name (required if registering" type="text"  required/>
-        <input placeholder="Profile pic URL (Optional)" />
-        <input placeholder="Email" type="email"  required/>
-        <input placeholder="Password" type="password" required />
+        <input placeholder="Full name (required if registering" type="text" value={name} onChange={(e) => setName(e.target.value)}  required/>
+        <input placeholder="Profile pic URL (Optional)" value={photoURL} onChange={(e) => setPhotoURL(e.target.value)}/>
+        <input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}  required/>
+        <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         <button type="submit" onClick={login }>Sing in</button>
       </form>
 
