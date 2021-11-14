75f7be93cba0277cdd069b964f6049fdabff86bf
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-14 12:27:26 -0600

    cerated a function that returns jsx

--- src/Components/Widgets/Widgets.js
+++ src/Components/Widgets/Widgets.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import "../Widgets/Widgets.css"
 import InfoIcon from "@material-ui/icons/Info"
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
 
 function Widgets() {
+
+    const newsArticles  =(heading, subtitle) => {
+        return <div className="widgets_articles">
+            <div className="widgets_articlesLeft">
+                <FiberManualRecordIcon />
+            </div>
+            <div className="widgets_articlesRight">
+                <h1>{heading}</h1>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    }
     return (
         <div className="widgets">
            <div className="widgets_header">
                <h2>LinkedIn News</h2>
                <InfoIcon />
            </div>
-            
+            {newsArticles("PAPA React is back", "Top news - 9099 readers")}
+             {newsArticles("Coronavirus: UK updates", "Top news - 886 readers")}
+              {newsArticles("Tesla hits new highs", "Cars & Auto - 300 readers")}
+               {newsArticles("Bitcoin Breaks $22k", "Crypto - 123 readers")}
+                {newsArticles("PAPA React is back", "Top news - 9099 readers")}
+                 {newsArticles("PAPA React is back", "Top news - 9099 readers")}
         </div>
     )
 }
