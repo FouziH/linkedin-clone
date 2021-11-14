7de2eedfbdc3bf8a07250d1e5d94ca7249835b78
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-14 11:57:36 -0600

    imported widgets to the application

---
+++ src/Components/Widgets/Widgets.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Widgets/Widgets.css"
+
+function Widgets() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            
+        </div>
+    )
+}
+
+export default Widgets
