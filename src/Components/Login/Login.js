66c91a412ed3c97becc518b6c878236d4e1178e4
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-13 23:18:39 -0600

    added styling to the form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { auth } from "../Firebase/Firebase";
 import "../Login/Login.css";
 
 export default function Login() {
-    const login = () => {
+    const login = (e) => {
+        e.preventDefault()
+        auth
 
     }
     const register = () => {
@@ -12,14 +15,14 @@ export default function Login() {
     <div className="login">
       <img src="https://cdn.worldvectorlogo.com/logos/linkedin.svg" alt="" />
       <form>
-        <input placeholder="Full name (required if registering" type="text" />
+        <input placeholder="Full name (required if registering" type="text"  required/>
         <input placeholder="Profile pic URL (Optional)" />
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
+        <input placeholder="Email" type="email"  required/>
+        <input placeholder="Password" type="password" required />
         <button type="submit" onClick={login }>Sing in</button>
       </form>
 
-      <p>Not a member? 
+      <p>Not a member? {" "}
 
           <span className="login_register" onClick={register}>Register Now</span>
       </p>
