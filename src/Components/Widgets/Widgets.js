9b18f863867e38a6291e94c804a099790036a7d8
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-14 13:15:16 -0600

    formatt

--- src/Components/Widgets/Widgets.js
+++ src/Components/Widgets/Widgets.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import "../Widgets/Widgets.css"
-import InfoIcon from "@material-ui/icons/Info"
-import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
+import React from "react";
+import "../Widgets/Widgets.css";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 function Widgets() {
-
-    const newsArticles  =(heading, subtitle) => {
-        return <div className="widgets_article">
-            <div className="widgets_articlesLeft">
-                <FiberManualRecordIcon />
-            </div>
-            <div className="widgets_articlesRight">
-                <h4>{heading}</h4>
-                <p>{subtitle}</p>
-            </div>
-        </div>
-    }
+  const newsArticles = (heading, subtitle) => {
     return (
-        <div className="widgets">
-           <div className="widgets_header">
-               <h2>LinkedIn News</h2>
-               <InfoIcon />
-           </div>
-            {newsArticles("Exec's back-to-office", "Top news - 9099 readers")}
-             {newsArticles("Americans quit again", "Top news - 886 readers")}
-              {newsArticles("Corporate giants", "Cars & Auto - 300 readers")}
-               {newsArticles("Bitcoin Breaks $22k", "Crypto - 123 readers")}
-                {newsArticles("Flight change", "Top news - 9099 readers")}
-                 {newsArticles("How to write a code", "Top news - 9099 readers")}
+      <div className="widgets_article">
+        <div className="widgets_articlesLeft">
+          <FiberManualRecordIcon />
         </div>
-    )
+        <div className="widgets_articlesRight">
+          <h4>{heading}</h4>
+          <p>{subtitle}</p>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className="widgets">
+      <div className="widgets_header">
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticles("Exec's back-to-office", "Top news - 9099 readers")}
+      {newsArticles("Americans quit again", "Top news - 886 readers")}
+      {newsArticles("Corporate giants", "Cars & Auto - 300 readers")}
+      {newsArticles("Bitcoin Breaks $22k", "Crypto - 123 readers")}
+      {newsArticles("Flight change", "Top news - 9099 readers")}
+      {newsArticles("How to write a code", "Top news - 9099 readers")}
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
