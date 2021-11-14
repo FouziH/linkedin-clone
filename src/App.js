fdd2b908e579b6c96eb8b485e009254437b9fc2d
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-14 09:54:48 -0600

    created ability to stay logged in even on refresh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { useSelector } from "react-redux";
 import Feed from "./Components/Feed/Feed";
@@ -6,9 +6,31 @@ import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { selectUser } from "./features/userSlice";
 import Login from "./Components/Login/Login";
+import { auth } from "./Components/Firebase/Firebase";
+import { useDispatch } from "react-redux";
+import { logout, login } from "./features/userSlice";
 
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        //user is logged in 
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL
+        }))
+
+      }else {
+        // user is logged out
+        dispatch(logout())
+      }
+    })
+  }, [])
   const user = useSelector(selectUser);
   return (
     <div className="app">
